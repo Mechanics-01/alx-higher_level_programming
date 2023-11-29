8cd82e3df788931fc3b5b6b325e3902600f6ce14
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-11-28 21:15:30 -0800

    -

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,6 +4,6 @@ def uppercase(str):
     for i in range(len(str)):
         uni = ord(str[i])
         if uni >= 97 and uni <= 122:
-            uni = uni -32
-        print("{}".format(chr(uni)), end = '')
+            uni = uni - 32
+        print("{}".format(chr(uni)), end='')
     print()
