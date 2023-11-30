663b5613d599a128e9430c0fa604e673718f1464
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-11-30 10:18:36 -0800

    added file

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    all = 0
+    for i in range(len(sys.argv) - 1):
+        all += int(sys.argv[i + 1])
+    print("{}".format(all))
